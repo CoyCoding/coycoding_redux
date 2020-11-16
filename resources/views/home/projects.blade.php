0ad264ab3b687294fa26e54fd5401803870a0a61
Coycoding <fluffytoycoy@gmail.com>
2020-11-16 09:54:45 -0600

    loads projects from database to view

--- resources/views/home/projects.blade.php
+++ resources/views/home/projects.blade.php
@@ -8,12 +8,40 @@
     <div class="min-wrapper">
       <div class="project-wrapper">
         <div class="projects-grid">
-          @foreach ($projects as $project)
-            {{$project->name}}
+          @foreach ($projects->reverse() as $project)
+            <div class="project">
+              <div class="square">
+                <div class="img-wrapper">
+                  <img src="{{ $project->img_url }}"/>
+                </div>
+              </div>
+              <div class="project-info">
+                  <div class="project-details">
+                    <h2>{{ $project->name }}</h2>
+                    <div class="info">
+                      <p> {{ $project->description }} </p>
+                      <div class="skills">
+                        @foreach ($project->bulletpoints as $bulletpoint)
+                          <div class="bullet-point"><i class="fas fa-circle"></i>{{$bulletpoint->text}}</div>
+                        @endforeach
+                      </div>
+                    </div>
+                    <div class="project-btns">
+                      <a target="_blank" href="{{$project->github_link}}">
+                        <div>
+                          source code
+                        </div>
+                      </a>
+                      <a target="_blank" href="{{$project->site_link}}">
+                        <div>
+                          demo
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+              </div>
+            </div>
           @endforeach
-        </div>
-        <div class="more-btn">
-          <p>more</p>
         </div>
       </div>
     </div>
