4c746d9a481458459bd485786203eb851d632b10
Coycoding <fluffytoycoy@gmail.com>
2020-11-16 19:17:05 -0600

    changed nav tags

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,22 +1,22 @@
 <section class="nav">
   <div class="nav-wrapper container">
     <div class="logo">
-      <a href="/">
+      <a href="/#">
         <img src="/images/logo.png" />
       </a>
       <p>Matt Coy</p>
     </div>
     <ul class="menu">
       <li>
-        <a href="#skills">Skills</a>
+        <a href="/#skills">Skills</a>
         <div class="hover-bg"></div>
       </li>
       <li>
-        <a href="#projects">Portfolio</a>
+        <a href="/#projects">Portfolio</a>
         <div class="hover-bg"></div>
       </li>
       <li>
-        <a href="#about">About Me</a>
+        <a href="/#about">About Me</a>
         <div class="hover-bg"></div>
       </li>
     </ul>
